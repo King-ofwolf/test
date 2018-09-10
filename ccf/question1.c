c053d1c540615cce2781758d7065229bbadd3d13
wangshengling <wangshengling897@163.com>
2018-09-10 17:35:57 +0800

    write question1.c

--- ccf/question1.c
+++ ccf/question1.c
@@ -2,7 +2,25 @@
 int main(void)
 {
 	/* code */
-	int array[40];
-	
+	int c=1,lastc=1;
+	int total=0;
+	while(c!=0){
+		scanf("%d",&c);
+		if(c==1) {
+			total+=1;
+			lastc=1;
+		}
+		else if (c==2){
+			if (lastc==1) {
+				total+=2;
+				lastc=2;
+			}
+			else {
+				total=total+lastc+2;
+				lastc+=2;
+			}
+		}
+	}
+	printf("%d",total);
 	return 0;
 }
